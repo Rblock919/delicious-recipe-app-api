66044fc2cc346167f022bf49d1ab113749dd52d7
Ryan Block <rblock1@uncc.edu>
2018-04-11 00:15:47 -0400

    Changed eslint un-def rule to warn and modified dbtest.js to implement async calls in both es2016+ and before methods

--- dbtest.js
+++ dbtest.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+var chalk = require('chalk');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -14,12 +15,20 @@ MongoClient.connect(url, function (err, client) {
 
     const db = client.db(dbName);
 
-    insertDocuments(db, function () {
-        findDocuments(db, function () {
-            client.close();
-        });
+    main(db);
+
+    //var docs = await findDocumentsNew(db);
+    findDocuments(db, function() {
+        client.close();
+        console.log(chalk.green('Done finding records in a non es2016+ fashion'));
     });
 });
+
+async function main(db) {
+    var docs = await findDocumentsNew(db);
+    console.log(docs);
+    console.log(chalk.green('Done Printing docs in an async es2016+ fashion'));
+}
 
 const insertDocuments = function (db, callback) {
     /*// Get the documents collection
@@ -37,6 +46,24 @@ const insertDocuments = function (db, callback) {
     callback();
 }
 
+const findDocumentsNew = function (db) {
+
+    return new Promise(function (fulfill, reject) {
+        // Get the documents collection
+        const collection = db.collection('recipes');
+        // Find some documents
+        collection.find({'title': 'Pineapple Poblano Beef Tacos'}).toArray(function (err, docs) {
+            //assert.equal(err, null);
+            if (err) {
+                reject(err);
+            }
+            console.log('Found the following records');
+            //console.log(docs)
+            fulfill(docs);
+        });
+    });
+}
+
 const findDocuments = function (db, callback) {
     // Get the documents collection
     const collection = db.collection('recipes');
@@ -44,7 +71,7 @@ const findDocuments = function (db, callback) {
     collection.find({'title': 'Pineapple Poblano Beef Tacos'}).toArray(function (err, docs) {
         assert.equal(err, null);
         console.log('Found the following records');
-        console.log(docs)
+        //console.log(docs)
         callback(docs);
     });
 }
