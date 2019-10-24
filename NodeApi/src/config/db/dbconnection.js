6c7bf8cc53ca99aabca0cbc5658cfb8471f680ea
Ryan Block <ryan.block93@gmail.com>
2019-10-23 22:41:07 -0400

    added back local/remote db uri options into dbconnection.js

--- NodeApi/src/config/db/dbconnection.js
+++ NodeApi/src/config/db/dbconnection.js
@@ -1,4 +1,7 @@
 //filler text
-const connectionString = process.env.DB_URI;
+const connectionString = {
+  local: process.env.DB_URI_LOCAL,
+  remote: process.env.DB_URI
+};
 
 module.exports = connectionString;
