a33bd980d9001faa421b2be7c609711896a0a819
Ryan Block <Ryan.Block93@gmail.com>
2020-03-15 13:25:36 -0400

    removed mongo-connect as a dependency because not technically using it anymore and it was breaking tsc build

--- src/config/session/sessionConfig.ts
+++ src/config/session/sessionConfig.ts
@@ -1,59 +1,59 @@
 // const session = require('express-session');
-import session from 'express-session';
-import getMongoStore from 'connect-mongo';
-import authConfig from '../auth/authConfig';
+// import session from 'express-session';
+// import getMongoStore from 'connect-mongo';
+// import authConfig from '../auth/authConfig';
 
-const { MongoStore } = getMongoStore(session);
+// const MongoStore = getMongoStore(session);
 
-// const authConfig = require('../auth/authConfig');
-// const MongoStore = require('connect-mongo')(session);
+// // const authConfig = require('../auth/authConfig');
+// // const MongoStore = require('connect-mongo')(session);
 
-function sessionConfig(app, mongoose) {
-  session.Session.prototype.login = (user, cb) => {
-    const { req } = this;
-    req.session.regenerate(err => {
-      if (err) {
-        cb(err);
-      } else {
-        req.session.userInfo = user;
-        cb();
-      }
-    });
-  };
+// function sessionConfig(app, mongoose) {
+  // session.Session.prototype.login = (user, cb) => {
+    // const { req } = this;
+    // req.session.regenerate(err => {
+      // if (err) {
+        // cb(err);
+      // } else {
+        // req.session.userInfo = user;
+        // cb();
+      // }
+    // });
+  // };
 
-  session.Session.prototype.logout = cb => {
-    const { req } = this;
-    req.session.destroy(err => {
-      if (err) {
-        cb(err);
-      } else {
-        cb();
-      }
-    });
-  };
+  // session.Session.prototype.logout = cb => {
+    // const { req } = this;
+    // req.session.destroy(err => {
+      // if (err) {
+        // cb(err);
+      // } else {
+        // cb();
+      // }
+    // });
+  // };
 
-  const mongooseStore = new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 7 * 24 * 60 * 60, // 1 week
-  });
+  // const mongooseStore = new MongoStore({
+    // mongooseConnection: mongoose.connection,
+    // ttl: 7 * 24 * 60 * 60, // 1 week
+  // });
 
-  app.use(
-    session({
-      store: mongooseStore,
-      secret: authConfig.sessionsSecret,
-      cookie: {
-        path: '/api',
-        httpOnly: true,
-        secure: JSON.parse(process.env.SECURE_COOKIES),
-        sameSite: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-      },
-      name: 'id',
-      resave: true,
-      rolling: true,
-      saveUninitialized: false,
-    })
-  );
-}
+  // app.use(
+    // session({
+      // store: mongooseStore,
+      // secret: authConfig.sessionsSecret,
+      // cookie: {
+        // path: '/api',
+        // httpOnly: true,
+        // secure: JSON.parse(process.env.SECURE_COOKIES),
+        // sameSite: true,
+        // maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+      // },
+      // name: 'id',
+      // resave: true,
+      // rolling: true,
+      // saveUninitialized: false,
+    // })
+  // );
+// }
 
-export default sessionConfig;
+// export default sessionConfig;
